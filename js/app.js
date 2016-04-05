26c35c139a9615262b0fb2a7725e05cd5422f5f8
Geoffrey Pay <geoffrey.pay@gmail.com>
2016-04-04 20:46:42 -0700

    Consolidated JS files
    Consolidates map.js and ui.js into app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,44 @@
+var minRange = 1940, 
+    maxRange = 1990,
+    lowerBound = 1920,
+    upperBound = 2010,
+    director,
+    map,
+    layer,
+    table = "1x8noYJ9JEr0g3ObbFG03cT9VbsRgAVpcyqbqS8k";
+    
+function initialize() {
+    
+    $('#min').text(lowerBound);
+    $('#max').text(upperBound);
+    
+    map = new google.maps.Map(document.getElementById('map-canvas'), {
+        center: new google.maps.LatLng(37.78217505967539, -122.41824985581053),
+        zoom: 13,
+        streetViewControl: true,
+        streetViewControlOptions: {
+            position: google.maps.ControlPosition.LEFT_TOP
+        },        
+        zoomControl: true,
+        zoomControlOptions: {
+          position: google.maps.ControlPosition.LEFT_TOP
+        },
+        scaleControl: false,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+    
+    layer = new google.maps.FusionTablesLayer({
+        query: {
+            select: "location",
+            from: "1x8noYJ9JEr0g3ObbFG03cT9VbsRgAVpcyqbqS8k",
+            where: "col1>="+minRange+" and col1<="+maxRange
+        },
+        map: map,
+        styleId: 5,
+        templateId: 4
+    });
+}
+
 $(function() {
     
     // Set first tab active by default 
@@ -502,7 +543,7 @@ $(function() {
 });
 
 function updateTitleMap() {
-//    console.log("value: " + title);
+    console.log("value: " + title);
     layer.setMap(); // clears markers
     layer = new google.maps.FusionTablesLayer({
         query: {
@@ -517,7 +558,7 @@ function updateTitleMap() {
 }    
 
 function updateDirMap() {
-//    console.log("value: " + director);
+    console.log("value: " + director);
     layer.setMap(); // clears markers
     layer = new google.maps.FusionTablesLayer({
         query: {
